a0b634e404e3a4c2284acde273877e36343a6979
bromine1 <rwstauffer72@gmail.com>
2022-02-28 10:02:42 -0500

    Initial Push

---
+++ Warmups/2/28 Wamup.py
@@ -0,0 +1,7 @@
+temp = float(input("What is the temperature today?"))
+if temp < 50:
+    print("Its is cold outside!")
+
+bodyTemp = float(input("What is your temperature"))
+if bodyTemp > 98.6:
+    print("You have a fever")
