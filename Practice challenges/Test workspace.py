e90545f9c742b2833b462bb71dd908880adf03be
bromine1 <rwstauffer72@gmail.com>
2022-02-28 10:17:16 -0500

    Update

--- Practice challenges/Test workspace.py
+++ Practice challenges/Test workspace.py
@@ -1,7 +1,2 @@
-temp = float(input("What is the temperature today?"))
-if temp < 50:
-    print("Its is cold outside!")
-
-bodyTemp = float(input("What is your temperature"))
-if bodyTemp > 98.6:
-    print("You have a fever")
+for x in range(8):
+    print(x)
