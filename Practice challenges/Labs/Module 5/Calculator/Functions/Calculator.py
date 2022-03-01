ad55ba5b915c671eb746b2027ffa3f74eb719a1e
bromine1 <rwstauffer72@gmail.com>
2022-03-01 15:24:04 -0500

    calculator push

--- Practice challenges/Labs/Module 5/Calculator/Functions/Calculator.py
+++ Practice challenges/Labs/Module 5/Calculator/Functions/Calculator.py
@@ -1,9 +1,16 @@
 def calculator():
+    # Initialize data and modules
     import decimal
     try:
-        num1 = decimal(input("Please input the first number"))
-        num2 = decimal(input("Please input the second number"))
+        a = decimal.Decimal(input("\nPlease input the first number: "))
+        b = decimal.Decimal(input("\nPlease input the second number: "))
     except:
         print("Something went wrong")
         calculator()
-    
+    print("\nPlease choose an operation:\n")
+    operations = {'add': a + b, 'subtract': a - b,'multiply': a * b, 'divide': a/b}
+    for x in operations.keys():
+        print (f"{x}\n")
+    return(operations[input("\nType the given name of your operation: ")])
+
+print(calculator())
