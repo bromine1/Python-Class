bcb107a267ee95d615b51fdd9c28563c6c7634d5
bromine1 <rwstauffer72@gmail.com>
2022-03-01 15:27:31 -0500

    Added comments to calculator

--- Practice challenges/Labs/Module 5/Calculator/Functions/Calculator.py
+++ Practice challenges/Labs/Module 5/Calculator/Functions/Calculator.py
@@ -1,16 +1,20 @@
 def calculator():
     # Initialize data and modules
     import decimal
+    # gather user input
     try:
         a = decimal.Decimal(input("\nPlease input the first number: "))
         b = decimal.Decimal(input("\nPlease input the second number: "))
     except:
         print("Something went wrong")
         calculator()
+    # select operator
     print("\nPlease choose an operation:\n")
     operations = {'add': a + b, 'subtract': a - b,'multiply': a * b, 'divide': a/b}
+    # print operator options
     for x in operations.keys():
         print (f"{x}\n")
+    # return operator output value
     return(operations[input("\nType the given name of your operation: ")])
 
 print(f"Your result is {calculator()}")
