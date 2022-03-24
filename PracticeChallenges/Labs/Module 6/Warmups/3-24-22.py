cb899cf9eff27072b76cf14bce11335df5e0ce60
bromine1 <rwstauffer72@gmail.com>
2022-03-24 12:55:34 -0400

    update warmup

--- PracticeChallenges/Labs/Module 6/Warmups/3-24-22.py
+++ PracticeChallenges/Labs/Module 6/Warmups/3-24-22.py
@@ -2,18 +2,20 @@
 
 str1 = " the Champions of NCAA will be known as the best of the best in the entire world"
 str1 = str1.strip("the")
+# remove the
 print (str1)
+#remove somethign else
 str1 = str1.strip("Champions")
 print (str1)
 
 str2 = " the best way to learn programming is be the best at practicing the examples daily"
-str2 = str2.strip("the")
+str2 = str2.strip("the") #remove the
 print(str2)
-str2 = str2.strip("best")
+str2 = str2.strip("best") #remove something else
 print(str2)
 
 str3 = " the teacher can be the greatest if the opportunity to teach daily is present"
-str3.strip("the")
+str3.strip("the") # remove the
 print(str3)
-str3.strip("teacher")
+str3.strip("teacher") #remove something else
 print(str3)
