53b24b511761dff03015a9fbaf26eb19b6b30d3b
bromine1 <rwstauffer72@gmail.com>
2022-03-24 14:50:32 -0400

    Update led

--- PracticeChallenges/Labs/Module 6/LED_Display.py
+++ PracticeChallenges/Labs/Module 6/LED_Display.py
@@ -1,86 +1,34 @@
+#Ryan Stauffer LED Display Lab
 
-def nums(numbers):
-    one = """
-     *
-     *
-     *
-     *
-     *
-    """
-    two = """
-    ***
-      *
-    ***
-    *
-    ***
-    """
-    three = """
-    ***
-      *
-    ***
-      *
-    ***
-    """
-    four = """
-    * *
-    * *
-    ***
-      *
-      *
-    """
-    five = """
-    ***
-    *
-    ***
-      *
-    ***
-    """
-    six = """
-    **
-    *
-    ***
-    * *
-    ***
-    """
-    seven = """
-    ***
-      *
-      *
-      *
-      *
-    """
-    eight = """
-    ***
-    * *
-    ***
-    * *
-    ***
-    """
-    nine = """
-    ***
-    * *
-    ***
-      *
-      *
-    """
-    zero = """
-    ***
-    * *
-    * *
-    * *
-    ***
-    """
-    nums ={
-        1 : one,
-        2 : two,
-        3 : three,
-        4 : four,
-        5 : five,
-        6 : six,
-        7 : seven,
-        8 : eight,
-        9 : nine,
-        0 : zero
+#I did look up a hint to the lab and got the following:
+# Hints taken from https://stackoverflow.com/a/58721716
+#Keep them on one line
+#use 3 dimensional lists
+
+
+
+def nums(num):
+    numbers = {
+        #Map each number line by line
+        #Array will essentially be rotated right 90 degrees, placing numbers correctly
+        
+        #Line:  1      2      3      4      5
+        '0': ("***", "* *", "* *", "* *", "***"),
+        '1': (" * ", " * ", " * ", " * ", " * " ),
+        '2': ("***", "  *", "***", "*  ", "***" ),
+        '3': ("***", "  *", "***", "  *", "***"),
+        '4': ("***", "***", "***", "***", "***"),
+        '5': ("***", "***", "***", "***", "***"),
+        '6': ("***", "***", "***", "***", "***"),
+        '7': ("***", "***", "***", "***", "***"),
+        '8': ("***", "***", "***", "***", "***"),
+        '9': ("***", "***", "***", "***", "***"),
+        '.': ("***", "***", "***", "***", "***")
     }
-    
-nums(1)
+    numberString = [numbers[nums] for nums in str(num)] #Creates a 3 dimensional list with each number
+    for n in range (5): # used for choosing which line is being printed
+        print(" ".join(part[n] for part in numberString)) # prints a line of the number. part in partstring selects the number
+        #part[n] selects the line from the selected number
+
+nums(123)
+
