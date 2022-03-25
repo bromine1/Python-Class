c41881dd1e8804342a093720cfdc2c40ea802ef2
bromine1 <rwstauffer72@gmail.com>
2022-03-25 12:57:26 -0400

    3-25 warmup

---
+++ PracticeChallenges/Labs/Module 6/Warmups/3-25-22.py
@@ -0,0 +1,8 @@
+str1 = "  Who will win the NCAA Championship? No one will know who will be the best in 2022."
+str2 = " Programming is easier to learn by practicing it daily."
+str3 = " With the support of my teacher, I feel supported often."
+
+
+print(str1.strip(" wW.2")) #remove much of the text
+print(str2.strip(" wW.2")) #Remove only the period
+print(str3.strip(" wW.2")) #remove only the start and period
