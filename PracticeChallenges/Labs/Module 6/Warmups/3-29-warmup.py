4f94de1e12cd1c1e2f074ad9e36d8fc105c6761d
bromine1 <rwstauffer72@gmail.com>
2022-03-29 12:54:24 -0400

    2-29 warmup

---
+++ PracticeChallenges/Labs/Module 6/Warmups/3-29-warmup.py
@@ -0,0 +1,62 @@
+#Ryan Stauffer
+#Period 4
+#3/29/2022
+#Python Programming II Warm-up Assignment
+
+#Instructions:
+#Step 1: Review the sample pieces of code carefully using the P.R.I.M.M. method. Use this link to access these two examples.
+
+#Step 2: Provide your prediction line by line answering the question below, "What is the expected output?".
+
+#Step 3: Run the program to see the correct output. Were you correct?
+
+#Step 4: Create a new programming solution with comments demonstrating your programming knowledge and critical thinking process.
+
+#Exercise #1: What is the expected output of the following code?
+#zero
+
+try:
+    print(1/0)
+except ZeroDivisionError:
+    print("zero")
+except ArithmeticError:
+    print("arith")
+except:
+    print("some")
+
+#Exercise #2: What is the expected output of the following code?
+#Arithmetic error
+
+try:
+    print(1/0)
+except ArithmeticError:
+    print("arith")
+except ZeroDivisionError:
+    print("zero")
+except:
+    print("some")
+
+
+#Exercise #3: What is the expected output of the following code?
+#some
+#Also we never learned assert, good to hear tho
+
+
+def foo(x):
+    assert x
+    return 1/x
+
+
+try:
+    print(foo(0))
+except ZeroDivisionError:
+    print("zero")
+except:
+    print("some")
+
+
+num = 0
+try:
+    1/num
+except:
+    print("Your math was wrong")
--- PracticeChallenges/Test workspace.py
+++ PracticeChallenges/Test workspace.py
@@ -1,6 +1,7 @@
-VideoMedia = ["Fullmetal ALchemist Brotherhood", "She-ra", "Ducktales", "SpaceBalls", "Monty Python and the holy grail"]
-print(f"The sorted list for my favorite movies: \n{sorted(VideoMedia)}, and the unsorted list: \n {VideoMedia}")
-snacks = ["Potato Chips", "Salsa", "Chocolate coverd espresso beans", "Applesauce", "bannanas"]
-print (snacks)
-snacks.sort()
-print(f"I sorted your snacks for you. \n {snacks} \n I hope those werent organised already")
+x = "hello"
+
+#if condition returns True, then nothing happens:
+assert x == "hello"
+
+#if condition returns False, AssertionError is raised:
+assert x == "goodbye"
