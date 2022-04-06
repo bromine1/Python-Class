b1a042a3fa93675148dd876e3db2d08fe3968498
bromine1 <70975324+bromine1@users.noreply.github.com>
2022-04-06 12:50:44 -0400

    Complete assignment

--- PracticeChallenges/Labs/Module7/StackObject.py
+++ PracticeChallenges/Labs/Module7/StackObject.py
@@ -1,29 +1,28 @@
-class Stack:  # Defining the Stack class.
-    def __init__(self):  # Defining the constructor function.
-        self.stack = []
-    
-    def push(self, val):
-        self.stack.append(val)
-    
-    def pop(self):
-        val = self.stack[-1]
-        del self.stack[-1]
-        return (val)
+class Stack:
+    def __init__(self):
+        self.__stk = []
 
-class AddingStack(Stack):
+    def push(self, val):
+        self.__stk.append(val)
+
+    def pop(self):
+        val = self.__stk[-1]
+        del self.__stk[-1]
+        return val
+class CountingStack(Stack):
     def __init__(self):
         Stack.__init__(self)
-    self.__sum = 0
+        self.count = 0
 
+    def get_counter(self):
+        return(self.count)
 
+    def pop(self):
+        self.count += 1
+        Stack.pop(self)
 
-stack_object = Stack()  # Instantiating the object.
-
-stack_object.push(3)
-stack_object.push(2)
-stack_object.push(1)
-
-
-print(stack_object.pop())
-print(stack_object.pop())
-print(stack_object.pop())
+stk = CountingStack()
+for i in range(100):
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter())
