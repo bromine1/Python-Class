4984b15988c9da139ebcd61b870a6ae5b59fcc73
bromine1 <rwstauffer72@gmail.com>
2022-04-12 15:38:02 -0400

    Add comments

--- PracticeChallenges/Labs/Module7/DarOfWeek.py
+++ PracticeChallenges/Labs/Module7/DarOfWeek.py
@@ -3,6 +3,7 @@ class WeekDayError(KeyError):
 	
 
 class Weeker:
+    #Reference tables for days and weeks
     __numToWeek = {
         1 : 'Sun',
         2 : 'Mon',
@@ -22,24 +23,27 @@ class Weeker:
         'Sat' : 7,
     }
 
-    def __init__(self, day):
+    def __init__(self, day): #get start date
         try:
             self.__day = Weeker.__weekToNum[day]
         except KeyError:
             raise WeekDayError
 
 
-    def __str__(self):
+    def __str__(self): #Account for date starting from 7 backwards and return it
         for dayOfWeek in reversed(range(1, 8)):
             if self.__day % dayOfWeek == 0:
                 return(str(Weeker.__numToWeek[dayOfWeek]))
 
-    def add_days(self, n):
+    def add_days(self, n): # add days
         self.__day += n % 7
+        #Modulous is needed to prevent numbers from getting large
+        #this should keep numbers in an acceptable range for __str__
 
-    def subtract_days(self, n):
+    def subtract_days(self, n): #subtracts days
         self.__day -= n % 7
-
+        #Modulous is needed to prevent numbers from getting large
+        #this should keep numbers in an acceptable range for __str__
 
 try:
     weekday = Weeker('Mon')
