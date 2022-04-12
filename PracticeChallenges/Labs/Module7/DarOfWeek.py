9c45edf5aa3483bf47e13dafd1b6005b1715b25d
bromine1 <rwstauffer72@gmail.com>
2022-04-12 14:41:09 -0400

    DayOfWeek lab

--- PracticeChallenges/Labs/Module7/DarOfWeek.py
+++ PracticeChallenges/Labs/Module7/DarOfWeek.py
@@ -1,29 +1,44 @@
-class WeekDayError(Exception):
+class WeekDayError(KeyError):
     pass
 	
 
 class Weeker:
-    day = {}
+    __numToWeek = {
+        1 : 'Sun',
+        2 : 'Mon',
+        3 : 'Tue',
+        4 : 'Wed',
+        5 : 'Thu',
+        6 : 'Fri',
+        7 : 'Sat'
+    }
+    __weekToNum = {
+        'Sun' : 1,
+        'Mon' : 2,
+        'Tue' : 3,
+        'Wed' : 4,
+        'Thu' : 5,
+        'Fri' : 6,
+        'Sat' : 7,
+    }
 
     def __init__(self, day):
-        #
-        # Write code here.
-        #
+        try:
+            self.__day = Weeker.__weekToNum[day]
+        except KeyError:
+            raise WeekDayError
+
 
     def __str__(self):
-        for x in reversed(range(1, 8)):
-            if day % x == 0:
-                print(day[x])
+        for dayOfWeek in reversed(range(1, 8)):
+            if self.__day % dayOfWeek == 0:
+                return(str(Weeker.__numToWeek[dayOfWeek]))
 
     def add_days(self, n):
-        #
-        # Write code here.
-        #
+        self.__day += n % 7
 
     def subtract_days(self, n):
-        #
-        # Write code here.
-        #
+        self.__day -= n % 7
 
 
 try:
