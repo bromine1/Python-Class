49a76118153cb64f08ac9e68876a678d8eeb2404
bromine1 <rwstauffer72@gmail.com>
2022-04-13 13:46:08 -0400

    add comments

--- PracticeChallenges/Labs/Module7/triangle.py
+++ PracticeChallenges/Labs/Module7/triangle.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 class Point:
-    def __init__(self, x=0.0, y=0.0):
+    def __init__(self, x=0.0, y=0.0): #initialize data
         self.__x = x
         self.__y = y
 
@@ -12,25 +12,25 @@ class Point:
         return self.__y
 
     def distance_from_xy(self, x, y):
-        return sqrt((self.__x - x)**2 + (self.__y - y)**2)
+        return sqrt((self.__x - x)**2 + (self.__y - y)**2) #Distance formula
 
     def distance_from_point(self, point):
         pointX = point.getx()
         pointY = point.gety()
-        return sqrt((self.__x - pointX)**2 + (self.__y - pointY)**2)
+        return sqrt((self.__x - pointX)**2 + (self.__y - pointY)**2) #distance formula
 
 
 class Triangle:
-    def __init__(self, vertice1, vertice2, vertice3):
+    def __init__(self, vertice1, vertice2, vertice3): # get points
         self.one = vertice1
         self.two = vertice2
         self.three = vertice3
 
-    def perimeter(self):
+    def perimeter(self): #Find distance between points
         one = self.one.distance_from_point(self.two)
         two = self.one.distance_from_point(self.three)
         three = self.two.distance_from_point(self.three)
-        return one + two + three
+        return one + two + three #return them
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
 print(triangle.perimeter())
