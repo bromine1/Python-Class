07011a77530ee47693f2b0e08c649d9da20036e6
bromine1 <70975324+bromine1@users.noreply.github.com>
2022-05-04 12:09:58 -0400

    commit and pull

---
+++ PracticeChallenges/Labs/Module8/Files/Histogram.py
@@ -0,0 +1,15 @@
+
+
+def countCharacter(text):
+    latinNum = {chr(k):0 for (k) in range(ord('a'), ord('z'))}  # Dictionary Comprehension of the alphabet
+    for line in text:
+        for character in line: #count characters
+            if character.isalpha():
+                latinNum[character.lower()] += 1 #add them to the dictionary
+    return latinNum
+
+with open('PracticeChallenges/Labs/Module8/Files/samplefile.txt', 'r', encoding='utf_8') as text:
+    histList = countCharacter(text)
+    for x in histList.items():
+        if x[1] > 0:
+            print(f"{x[0]} 🠖 {x[1]}")
